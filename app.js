d0ec1f0a542f4815990a30957b54b2dbaa3c41c9
“sulemanmukati” <sulemanmukati@gmail.com>
2024-03-04 01:08:39 -0800

    complete assigment

---
+++ app.js
@@ -0,0 +1,132 @@
+
+
+// Chapter 15 (Array I)
+// Q1
+var futureStudent =[]
+
+// Q2
+var Studentname =["suleman","hasnain","owais"]
+
+// Q3
+var alphabet = ["h", "i", "j", "k"];
+alert(alphabet[2]);
+
+var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+console.log(alphabet.length);
+
+var myArray = ["firstElement"];
+myArray.push("secondElement");
+alert(myArray[1]);
+
+
+// Chapter 16 (Array II)
+// Q1
+var arr =["suleman","ahmed","owais","hasnain","amman"]
+arr.push("muqaram");
+document.write(arr+ "<br>");
+// Q2
+var alphabet = ["h","i","j","k",]
+alphabet.pop("");
+document.write(alphabet);
+
+// Q3
+var alphabet = ["h","i","j","k",]
+alphabet.push("l");
+alphabet.push(5)
+document.write("<br>"+alphabet);
+
+
+// Chapter 16 (Array III)
+// Q1 
+var sizes = ["S","M", "XL", "XXL", "XXXL"]
+sizes.shift("")
+document.write("<br>"+sizes);
+
+// Q2
+var sizes = ["S","M", "XL", "XXL", "XXXL"]
+sizes.unshift(18,20,22)
+document.write("<br>"+sizes);
+
+// Q3
+var element = ["sir faraz"]
+element.push("sir ali")
+document.write("<br>"+element[0]) 
+
+// Q4
+var sizes = ["S","M", "XL", "XXL", "XXXL"]
+sizes.splice(2,"M","L",)
+document.write("<br>" + sizes)
+ 
+// Q5
+var sizes = ["S","M", "XL", "XXL", "XXXL"]
+var regsizes = sizes.slice(0,3)
+document.write("<br>" + regsizes)
+
+// Q6
+var pets = ["dog","cat","ox","duck","frog"]
+pets.splice(1,)
+pets.push("cow","goat")
+document.write("<br>" + pets)
+ 
+// Q7
+var pets = ["dog","cat","ox","duck","frog"]
+pets.splice(1,2)
+document.write("<br>"+pets) 
+
+// Q8
+var pets1 = ["dog","cat","ox","duck","frog","flea"]
+pets1.splice(3,2)
+document.write("<br>"+pets1) 
+
+
+
+
+//chapter 14 - 16
+
+// Q1
+var futureStudent =[]
+
+// Q2
+var Studentname =["suleman","hasnain","owais"]
+
+// Q3
+var string =["sir Ali"]
+
+// Q4
+var number =[1 ,2 ,4]
+
+// Q5
+var bollone =[false ,true]
+
+// Q6
+var mix =["sir Faraz" , 1 ,false , "owais" ]
+
+// Q7
+var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+for(let i=1;i<=8;i++)
+{
+  console.log(i +")"+ qualifications + "<br>" );
+  
+}
+
+// Q8
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
